982f68dd0f57ada0bf0a304d7bca10d76a2e1da9
ryota <sigurebidesu@yahoo.co.jp>
2020-05-14 18:17:08 +0900

    linbot コントローラー編集

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -3,14 +3,8 @@ class LinebotController < ApplicationController
 
   protect_from_forgery :except => [:callback]
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-
   def callback
+    @tasks = Task.all
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
@@ -21,18 +15,30 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each do |event|
+      if event.message['text'].include?('月曜日')
+        response = @tasks.content
+      end
+      
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
-            text: event.message['text']
+            text: response
           }
           client.reply_message(event['replyToken'], message)
         end
       end
     end
     head :ok
+  end
+
+  private
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
