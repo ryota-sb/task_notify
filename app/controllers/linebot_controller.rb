7d0ff19b495ee26f4452c34fcdf156ad8a10d18e
ryota <sigurebidesu@yahoo.co.jp>
2020-06-12 21:52:38 +0900

    userId出力 確認用

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,15 +5,15 @@ class LinebotController < ApplicationController
   protect_from_forgery :except => [:callback]
 
   # 渡されたweekのタスクをリストに格納
-  def week_tasks(week) 
-    tasks = Task.select(:content, :notification_time).order(:notification_time).where(is_done: false, week: week)
+  def week_tasks(week, user_id)
+    tasks = Task.select(:content, :notification_time).order(:notification_time).where(is_done: false, week: week, user_id: user_id)
     task_lists = tasks.map { |task| "スタート#{task.notification_time.strftime('%H:%M')} / #{task.content}" }
     return task_lists
   end
 
   # 完了タスクを未完了にする(リセット)
-  def reset_tasks(week)
-    tasks = Task.where(is_done: true, week: week)
+  def reset_tasks(week, user_id)
+    tasks = Task.where(is_done: true, week: week, user_id: user_id)
     tasks.update_all(is_done: false)
     return '今日のタスクをリセットしました。明日も頑張りましょう！'
   end
@@ -50,6 +50,8 @@ class LinebotController < ApplicationController
           client.reply_message(event['replyToken'], message_array)
         end
       end
+      userId = event['source']['userId']
+      p 'userId': userId
     end
     head :ok
   end
