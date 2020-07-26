7e1a3785caf4555516be1c2d2a34142fbfc52478
ryota <sigurebidesu@yahoo.co.jp>
2020-07-26 09:39:54 +0900

    定期処理 停止

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,6 @@ set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
 
 # 処理の実行ペース
-every 1.minutes do
-  rake 'push_line:push_line_message_tasks'
-end
+# every 1.minutes do
+#   rake 'push_line:push_line_message_tasks'
+# end
